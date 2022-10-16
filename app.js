3328287175ba682092eeb698b10fb025c53257cf
Mohammad Rezaei <mohammadofficialmail@gmail.com>
2022-10-16 11:42:55 +0330

    Changes added

---
+++ app.js
@@ -0,0 +1,13 @@
+const myName = "Mohammad";
+
+const myF = () => {
+  console.log(myName);
+};
+
+const ID = 1640359281;
+
+class myClass {
+    sayMyname() {
+        console.log("Mohammad")
+    }
+}
